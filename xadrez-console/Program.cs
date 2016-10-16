d8f54afd3744875349bb1a604bb34e797e27d77d
Augusto Melo <augustinho.melo@hotmail.com>
2016-10-16 17:58:51 -0200

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -37,8 +37,9 @@ namespace xadrez_console {
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
 
-                    
 
             }
             catch (TabuleiroException e) {
